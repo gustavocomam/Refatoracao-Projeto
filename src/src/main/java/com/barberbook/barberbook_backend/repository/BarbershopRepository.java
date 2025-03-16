e09668ad0fcd34b977dee69cdf7cbee563927025
gustavo <129975733+gustavocomam@users.noreply.github.com>
2025-03-16 19:58:43 -0300

    Delete src/src directory

--- src/src/main/java/com/barberbook/barberbook_backend/repository/BarbershopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.barberbook.barberbook_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.barberbook.barberbook_backend.model.Barbershop;
-
-@Repository
-public interface BarbershopRepository extends JpaRepository<Barbershop, Integer> {
-}
