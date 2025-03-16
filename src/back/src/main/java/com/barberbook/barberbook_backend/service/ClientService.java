ebf00509eb94de3dbd33551a6cde8aa9e5d674dd
gustavo <129975733+gustavocomam@users.noreply.github.com>
2025-03-16 20:27:05 -0300

    Update ClientService.java

--- src/back/src/main/java/com/barberbook/barberbook_backend/service/ClientService.java
+++ src/back/src/main/java/com/barberbook/barberbook_backend/service/ClientService.java
@@ -2,14 +2,22 @@ package com.barberbook.barberbook_backend.service;
 
 import com.barberbook.barberbook_backend.model.Client;
 import com.barberbook.barberbook_backend.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class ClientService {
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private static ClientService INSTANCE;
+    private final ClientRepository clientRepository;
+
+    private ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public static synchronized ClientService getInstance(ClientRepository repository) {
+        if (INSTANCE == null) {
+            INSTANCE = new ClientService(repository);
+        }
+        return INSTANCE;
+    }
 
     public Client saveClient(Client client) {
         return clientRepository.save(client);
@@ -21,9 +29,5 @@ public class ClientService {
 
     public void deleteClient(int id) {
         clientRepository.delete(findClientById(id));
-    }
-
-    public Iterable<Client> findAllClients() {
-        return clientRepository.findAll();
     }
 }
